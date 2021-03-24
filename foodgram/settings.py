6aa620c55ff5abe5c740cb488ffc89a0323ea9d8
4dragunov <4dragunov@gmail.com>
2021-03-24 10:29:22 +0300

    media

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -108,6 +108,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'static')
 # ]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Login
 LOGIN_URL = '/auth/login/'
